f80742c1886b80b641e1e1e6874b29dd43ad8ae7
Mihir-Shah-Tatvasoft <mihir.shah@tatvamail.com>
2023-01-17 14:00:18 +0530

    sidebar initial commit

--- vr-theme-4/src/Components/Sidebar/index.tsx
+++ vr-theme-4/src/Components/Sidebar/index.tsx
@@ -1,4 +1,29 @@
+import { Image, List, ListItem, Text } from "@chakra-ui/react";
+import { NavLink } from "react-router-dom";
+import { docsIcon } from "../../assets/images";
+
 const Sidebar: React.FC = () => {
-  return <p>sidebar</p>;
+
+  function closeSidebar() {
+    document.body.classList.remove("open-sidebar");
+  }
+
+  return (
+    <>
+    <List className="sidebar">
+    <ListItem>
+          <NavLink
+            to="/innerpage"
+            title="Users"
+            className="nav-link"
+            onClick={closeSidebar}
+          >
+            <Image src={docsIcon} alt="Users" />
+            <Text as="span">Users</Text>
+          </NavLink>
+        </ListItem>
+    </List>
+    </>
+  );
 };
 export default Sidebar;
