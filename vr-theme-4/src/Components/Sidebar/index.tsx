aca7452e0b879511de31b7f26f793ed402d1b137
Mihir-Shah-Tatvasoft <mihir.shah@tatvamail.com>
2023-01-17 15:10:17 +0530

    sidebar desktop initial commit

--- vr-theme-4/src/Components/Sidebar/index.tsx
+++ vr-theme-4/src/Components/Sidebar/index.tsx
@@ -1,6 +1,6 @@
 import { Image, List, ListItem, Text } from "@chakra-ui/react";
-import { NavLink } from "react-router-dom";
-import { docsIcon } from "../../assets/images";
+import { Link, NavLink } from "react-router-dom";
+import { dashboardActiveIcon, dashboardIcon, docsIcon, helpIcon, logo, logoPng, settingIcon, timelineIcon, userIcon } from "../../assets/images";
 
 const Sidebar: React.FC = () => {
 
@@ -10,19 +10,80 @@ const Sidebar: React.FC = () => {
 
   return (
     <>
-    <List className="sidebar">
-    <ListItem>
-          <NavLink
-            to="/innerpage"
-            title="Users"
-            className="nav-link"
-            onClick={closeSidebar}
-          >
-            <Image src={docsIcon} alt="Users" />
-            <Text as="span">Users</Text>
-          </NavLink>
-        </ListItem>
-    </List>
+      <div className="sidebar">
+        <Link to="#" title="Company" className="logo"><Image src={logoPng} /></Link>
+        <List>
+          <ListItem>
+            <NavLink
+              to="/dashboard"
+              title="Dashboard"
+              className="nav-link"
+              onClick={closeSidebar}
+            >
+              <span className="img-wrapper">
+                <Image src={dashboardIcon} className="default-icon"/>
+                <Image src={dashboardActiveIcon} className="active-icon"/>
+              </span>
+              <Text as="span">Dashboard</Text>
+            </NavLink>
+          </ListItem>
+          <ListItem>
+            <NavLink
+              to="/innerpage"
+              title="Users"
+              className="nav-link"
+              onClick={closeSidebar}
+            >
+              <Image src={userIcon} alt="Users" />
+              <Text as="span">Users</Text>
+            </NavLink>
+          </ListItem>
+          <ListItem>
+            <NavLink
+              to="/"
+              title="Docs"
+              className="nav-link"
+              onClick={closeSidebar}
+            >
+              <Image src={docsIcon} alt="Docs" />
+              <Text as="span">Docs</Text>
+            </NavLink>
+          </ListItem>
+          <ListItem>
+            <NavLink
+              to="/"
+              title="Timeline"
+              className="nav-link"
+              onClick={closeSidebar}
+            >
+              <Image src={timelineIcon} alt="Timeline" />
+              <Text as="span">Timeline</Text>
+            </NavLink>
+          </ListItem>
+          <ListItem>
+            <NavLink
+              to="/"
+              title="Help"
+              className="nav-link"
+              onClick={closeSidebar}
+            >
+              <Image src={helpIcon} alt="Help" />
+              <Text as="span">Help</Text>
+            </NavLink>
+          </ListItem>
+          <ListItem>
+            <NavLink
+              to="/"
+              title="Settings"
+              className="nav-link"
+              onClick={closeSidebar}
+            >
+              <Image src={settingIcon} alt="Settings" />
+              <Text as="span">Settings</Text>
+            </NavLink>
+          </ListItem>
+        </List>
+      </div>
     </>
   );
 };
--- vr-theme-4/src/assets/images/index.tsx
+++ vr-theme-4/src/assets/images/index.tsx
@@ -1,8 +1,10 @@
-export const loginLogo = require("./logo-logo.png");
-export const logo = require("./logo.png");
+export const dashboardIcon = require("./dashboard-default-ic.svg").default;
+export const dashboardActiveIcon = require("./dashboard-active-ic.svg").default;
 export const userIcon = require("./user-default-ic.svg").default;
 export const docsIcon = require("./docs-default-ic.svg").default;
-export const timlineIcon = require("./timeline-default-ic.svg").default;
+export const timelineIcon = require("./timeline-default-ic.svg").default;
 export const helpIcon = require("./help-default-ic.svg").default;
 export const settingIcon = require("./setting-default-ic.svg").default;
+export const logo = require("./logo.svg").default;
+export const logoPng = require("./logo.png");
 
