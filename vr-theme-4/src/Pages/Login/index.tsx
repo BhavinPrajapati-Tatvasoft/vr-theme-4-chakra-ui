27a4a4279049af03dcc606c868eed9754b9ed6f1
Bhavin Prajapati <bhavin.prajapati@tatvasoft.com>
2023-01-17 14:13:09 +0530

    login setup

--- vr-theme-4/src/Pages/Login/index.tsx
+++ vr-theme-4/src/Pages/Login/index.tsx
@@ -6,7 +6,16 @@ const Login = () => {
       <Helmet>
         <title>Login</title>
       </Helmet>
-      <div className="login-wrapper"></div>
+      <div className="login-wrapper">
+      <Grid templateColumns="repeat(12, 1fr)">
+      <GridItem colSpan={{ base: 12, lg: 6 }} >
+        left Content
+      </Grid>
+      <GridItem colSpan={{ base: 12, lg: 6 }} >
+      Right Content
+      </Grid>
+      </Grid>
+      </div>
     </>
   );
 };
